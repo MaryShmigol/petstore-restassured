86f093e154fac94682e450c2a709e48c8e061f02
Mary Shmigol <maryashmigol@gmail.com>
2019-03-17 16:05:16 +0300

    add method specification

--- src/test/java/Config.java
+++ src/test/java/Config.java
@@ -1,3 +1,5 @@
 public class Config {
     public static final String PET_STORE_BASE_HOST = "https://petstore.swagger.io/v2";
+
+    public static final String GET_PET_ENDPOINT = "/pet/{id}";
 }
--- src/test/java/RetStoreTests.java
+++ src/test/java/RetStoreTests.java
@@ -1,17 +1,16 @@
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
 public class RetStoreTests {
     @Test
     public void getPetById200() {
-     RestAssured
-                .given().baseUri(Config.PET_STORE_BASE_HOST)
-                .basePath("/pet/1")
-                .log().uri()
+        baseSpecification()
+                .basePath(Config.GET_PET_ENDPOINT)
+                .pathParam("id",1 )
                 .get()
                 .then()
                 .statusCode(200);
-
     }
 
     @Test
@@ -21,5 +20,10 @@ public class RetStoreTests {
                 .get()
                 .then()
                 .statusCode(404);
+    }
+    private RequestSpecification baseSpecification(){
+        return RestAssured.given()
+                   .baseUri(Config.PET_STORE_BASE_HOST)
+                   .log().uri();
     }
 }
